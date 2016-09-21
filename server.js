cbf631b2c28f0d0a11c4adf306306d8128707b47
chede_yugandhar <yogichede159@gmail.com>
2016-09-21 15:10:29 +0530

    Done the POST Request using Underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser')
+var _          = require('underscore');
 var app = express();
 var todos = [];
 var todosId = 1;
@@ -14,18 +15,13 @@ app.get('/',function(req,res){
 
  //GET Request
 app.get('/todos',function(req,res){
-  res.json(todos)
+  res.json(todos);
 })
 
 //GET Request
 app.get('/todos/:id',function(req,res){
     var todoId = parseInt(req.params.id,10);
-    var matchedRecord;
-    todos.forEach(function(key){
-      if(todoId === key.id){
-        matchedRecord=key;
-      }
-    });
+    var matchedRecord = _.findWhere(todos,{id:todoId});
     if(matchedRecord){
       res.json(matchedRecord);
     }
@@ -37,7 +33,10 @@ app.get('/todos/:id',function(req,res){
 //PORT Request
 app.post('/todos',function(req,res){
   var body = req.body;
-  //console.log('description : ' +body.description);
+  if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length===0){
+    return res.status(400).send();
+  }
+
   body.id = todosId++;
   todos.push(body);
   res.json(body);
