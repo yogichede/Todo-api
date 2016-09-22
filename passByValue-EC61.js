2cc07e13a337de651db790bc90a507f876e27a8c
chede_yugandhar <yogichede159@gmail.com>
2016-09-22 16:43:41 +0530

    Add passby value

---
+++ passByValue-EC61.js
@@ -0,0 +1,24 @@
+// var personDetails = {
+//   name : "yogi",
+//   age  : 25
+// };
+//
+// function updateRecord (obj){
+//   // obj = {
+//   //   name : "yogichede",
+//   //   age : 22
+//   // };
+//   obj.age=22;
+// }
+// updateRecord(personDetails);
+// console.log(personDetails);
+
+var personDetails = [2,3,4];
+
+function updateRecords (obj){
+  obj.push(8);
+  debugger;
+}
+
+updateRecords(personDetails);
+console.log(personDetails);
